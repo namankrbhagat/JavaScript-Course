ddfa96b1dc412510be54b089110026aae93ddf79
namankrbhagat <namankumarbhagat.4226@gmail.com>
2025-01-04 22:26:54 +0530

    save it

--- Data Structues Operators/script.js
+++ Data Structues Operators/script.js
@@ -53,6 +53,68 @@ const restaurant = {
   },
 };
 
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days`;
+
+for(const day of properties){
+  console.log(day);
+}
+
+//Property Value
+const values = Object.values(openingHours);
+console.log(values);
+
+//Entire Object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for(const [day, {open,close}] of entries){
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+
+
+/*
+///////////////////////////////////////
+// Optional Chaining
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('user array empty');
+
+
+
+
+
+///////////////////////////////////////
+// The for-of Loop
+
 
 const menu = [...restaurant.starterMenu,...restaurant.mainMenu];
 
@@ -63,7 +125,11 @@ for(const item of menu){
 for(const [i,el] of menu.entries()){
   console.log(`${i+1}: ${el} `);
 }
-/*
+
+
+
+
+
 ///////////////////////////////////////
 // Logical Assignment Operators
 const rest1 = {
